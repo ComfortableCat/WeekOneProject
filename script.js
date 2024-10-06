660e477dd599a94fb7233b2f16e7e03bc05a6923
Bertie Raffle <bertierwork@gmail.com>
2024-10-06 16:11:43 +0100

    add audio

--- script.js
+++ script.js
@@ -7,8 +7,12 @@ screenTop
 console.log(screenTop);
 */
 const topBtn = document.getElementsByClassName("backToTop")[0];
-
 function backToTop() {
   window.scrollTo(0, document.querySelector(".moon"));
 }
 topBtn.addEventListener("click", backToTop);
+
+let audio = document.createElement("audio");
+audio.src = "./HoliznaCC0 - Space!.mp3";
+audio.autoplay = true;
+audio.loop = true;
